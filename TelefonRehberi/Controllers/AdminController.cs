7a84ab7090337d2d59adcf5da01400e9481c3906
kadirsefaunal <kadir-unal@outlook.com>
2017-02-28 15:56:50 +0300

    Veri tabanı modeli eklendi.

--- TelefonRehberi/Controllers/AdminController.cs
+++ TelefonRehberi/Controllers/AdminController.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TelefonRehberi.Models;
 
 namespace TelefonRehberi.Controllers
 {
     public class AdminController : Controller
     {
+        TelefonRehberiDBEntities db = new TelefonRehberiDBEntities();
         // GET: Admin
         public ActionResult Index()
         {
+            
             return View();
         }
     }
