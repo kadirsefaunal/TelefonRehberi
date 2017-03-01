af86343d85d1baf6cca33bee836325a043684d08
kadirsefaunal <kadir-unal@outlook.com>
2017-03-01 12:36:54 +0300

    Admin giriş ekranı tamamlandı Close #7

--- TelefonRehberi/Controllers/PublicController.cs
+++ TelefonRehberi/Controllers/PublicController.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TelefonRehberi.Models;
+using TelefonRehberi.ViewModel;
 
 namespace TelefonRehberi.Controllers
 {
     public class PublicController : Controller
     {
+        private readonly TelefonRehberiDBEntities db = new TelefonRehberiDBEntities();
+        private VM vm = new VM();
+
         // GET: Public
         public ActionResult Index()
         {
---
+++ TelefonRehberi/ViewModel/VM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TelefonRehberi.Models;
+
+namespace TelefonRehberi.ViewModel
+{
+    public class VM
+    {
+        public Admin admin { get; set; }
+    }
+}
