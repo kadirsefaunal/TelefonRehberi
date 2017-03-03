5933796c5a899df5f573aac2f679c2957f102af4
kadirsefaunal <kadir-unal@outlook.com>
2017-03-04 00:39:32 +0300

    Yorum satırları eklendi.

--- TelefonRehberi/Controllers/AdminController.cs
+++ TelefonRehberi/Controllers/AdminController.cs
@@ -24,6 +24,10 @@ namespace TelefonRehberi.Controllers
             return RedirectToAction("Index", "Public");
         }
         
+        /// <summary>
+        /// Çalışan ekleme sayfasına yönlendirir
+        /// </summary>
+        /// <returns></returns>
         public ActionResult CalisanEkle()
         {
             if (Request.Cookies["KullaniciKimligi"] != null)
@@ -35,12 +39,22 @@ namespace TelefonRehberi.Controllers
             return RedirectToAction("Index", "Public");
         }
 
+        /// <summary>
+        /// Çalışanı veritabanına ekler
+        /// </summary>
+        /// <param name="calisan"></param>
+        /// <returns></returns>
         public JsonResult CEkle(Calisanlar calisan)
         {
             string sonuc = IslemCalisan.CalisanEkle(calisan);
             return Json(sonuc);
         }
 
+        /// <summary>
+        /// Çalışan silme ekranını açar ve silinecek çalışanın bilgilerini gösterir
+        /// </summary>
+        /// <param name="calisanID"></param>
+        /// <returns></returns>
         public ActionResult CalisanSil(int calisanID)
         {
             if (Request.Cookies["KullaniciKimligi"] != null)
@@ -58,12 +72,22 @@ namespace TelefonRehberi.Controllers
             return RedirectToAction("Index", "Public");
         }
 
+        /// <summary>
+        /// ID'si gelen çalışanı koşulu sağlıyorsa veritabanından siler
+        /// </summary>
+        /// <param name="calisanID"></param>
+        /// <returns></returns>
         public JsonResult CalisanSill(int calisanID)
         {
             string sonuc = IslemCalisan.CalisanSil(calisanID);
             return Json(sonuc);
         }
 
+        /// <summary>
+        /// Çalışan düzenle ekranını açar ve çalışana ait bilgileri gösterir
+        /// </summary>
+        /// <param name="calisanID"></param>
+        /// <returns></returns>
         public ActionResult CalisanDuzenle(int calisanID)
         {
             if (Request.Cookies["KullaniciKimligi"] != null)
@@ -81,12 +105,21 @@ namespace TelefonRehberi.Controllers
             return RedirectToAction("Index", "Public");
         }
         
+        /// <summary>
+        /// Çalışana ait yapılan değişiklikleri veritabanına kaydeder
+        /// </summary>
+        /// <param name="calisan"></param>
+        /// <returns></returns>
         public JsonResult CalisanDuzenlee(Calisanlar calisan)
         {
             string sonuc = IslemCalisan.CalisanDuzenle(calisan);
             return Json(sonuc);
         }
 
+        /// <summary>
+        /// Departmanların listelendiği sayfayı açar
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Departmanlar()
         {
             if (Request.Cookies["KullaniciKimligi"] != null)
@@ -97,6 +130,10 @@ namespace TelefonRehberi.Controllers
             return RedirectToAction("Index", "Public");
         }
 
+        /// <summary>
+        /// Departman ekleme sayfasını açar
+        /// </summary>
+        /// <returns></returns>
         public ActionResult DepartmanEkle()
         {
             if (Request.Cookies["KullaniciKimligi"] != null)
@@ -106,12 +143,22 @@ namespace TelefonRehberi.Controllers
             return RedirectToAction("Index", "Public");
         }
 
+        /// <summary>
+        /// Gelen departman eğer veritabanında bulunmuyorsa veritabanına kaydeder
+        /// </summary>
+        /// <param name="departman"></param>
+        /// <returns></returns>
         public JsonResult DepartmanEklee(Departmanlar departman)
         {
             string sonuc = IslemDepartman.DepartmanEkle(departman);
             return Json(sonuc);
         }
 
+        /// <summary>
+        /// Departmanı sil ekranını açar ve silinecek departmanın bilgilerini gösterir
+        /// </summary>
+        /// <param name="departmanID"></param>
+        /// <returns></returns>
         public ActionResult DepartmanSil(int departmanID)
         {
             if (Request.Cookies["KullaniciKimligi"] != null)
@@ -132,12 +179,22 @@ namespace TelefonRehberi.Controllers
             return RedirectToAction("Index", "Public");
         }
 
+        /// <summary>
+        /// Seçilen departmanı eğer şartları sağlıyorsa veritabanından siler
+        /// </summary>
+        /// <param name="departmanID"></param>
+        /// <returns></returns>
         public JsonResult DepartmanSill(int departmanID)
         {
             string sonuc = IslemDepartman.DepartmanSil(departmanID);
             return Json(sonuc);
         }
 
+        /// <summary>
+        /// Departman düzenleme sayfasını açar ve departmanın bilgilerini gösterir
+        /// </summary>
+        /// <param name="departmanID"></param>
+        /// <returns></returns>
         public ActionResult DepartmanDuzenle(int departmanID)
         {
             if (Request.Cookies["KullaniciKimligi"] != null)
@@ -158,12 +215,23 @@ namespace TelefonRehberi.Controllers
             return RedirectToAction("Index", "Public");
         }
 
+        /// <summary>
+        /// Düzenlenen departmanı veritabanına kaydeder
+        /// </summary>
+        /// <param name="departman"></param>
+        /// <returns></returns>
         public JsonResult DepartmanDuzenlee(Departmanlar departman)
         {
             string sonuc = IslemDepartman.DepartmanGuncelle(departman);
             return Json(sonuc);
         }
 
+        /// <summary>
+        /// Adminin şifresini değiştirme işlemini gerçekleştirir
+        /// </summary>
+        /// <param name="eskiParola"></param>
+        /// <param name="yeniParola"></param>
+        /// <returns></returns>
         public JsonResult SifreDegistir(string eskiParola, string yeniParola)
         {
             try
